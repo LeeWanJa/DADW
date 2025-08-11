b584687450b17d8c6ff4207c187470830582297f
이재완 <jjjkuul@naver.com>
2025-08-11 13:31:03 +0900

    예제 2-2 1월 입출금 내역 합계 계산하기

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class Main {
@@ -11,25 +14,18 @@ public class Main {
         final Path path = Paths.get(RESOURCES + "DADW.csv");
         final List<String> lines = Files.readAllLines(path);
         double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         for(final String line : lines){
             System.out.println(line);
             final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
-            System.out.println("total = " + total);
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            System.out.println(date);
+            if(date.getMonth() == Month.JANUARY){
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
         }
 
-        System.out.println("The total for all transactions is " + total);
-
-        /*
-            <기능>
-            콤마로 열 분리 -> 금액 추출 -> 금액을 double로 파싱
-
-            <한계>
-            파일이 비어있다면?
-            데이터에 문제가 있어 파싱하지 못한다면?
-            행의 데이터가 완벽하지 않다면?
-            ...
-         */
+        System.out.println("The total for all transactions in January is " + total);
     }
 }
