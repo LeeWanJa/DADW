d835414fe8de722b02a5ea558b57c2c8ea860954
이재완 <jjjkuul@naver.com>
2025-08-11 12:42:12 +0900

    예제 2-1 기능 및 한계점 주석 추가

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,16 @@ public class Main {
         }
 
         System.out.println("The total for all transactions is " + total);
+
+        /*
+            <기능>
+            콤마로 열 분리 -> 금액 추출 -> 금액을 double로 파싱
+
+            <한계>
+            파일이 비어있다면?
+            데이터에 문제가 있어 파싱하지 못한다면?
+            행의 데이터가 완벽하지 않다면?
+            ...
+         */
     }
 }
