03799000a45da5a29607d9f7403840ca8b2e33fc
이재완 <jjjkuul@naver.com>
2025-08-11 12:38:08 +0900

    예제 2-1 모든 거래 내역의 합 계산하기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
+public class Main {
+    private static final String RESOURCES = "src\\resources\\";
+
+    public static void main(String[] args) throws IOException {
+        final Path path = Paths.get(RESOURCES + "DADW.csv");
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        for(final String line : lines){
+            System.out.println(line);
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+            System.out.println("total = " + total);
+        }
+
+        System.out.println("The total for all transactions is " + total);
     }
 }
