a7260841a912179fa64f7b4b4bc0f96333d2cbce
이재완 <jjjkuul@naver.com>
2025-08-13 11:40:21 +0900

    2-14 예제 구현

---
+++ src/BankStatementCSVParserTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankStatementCSVParserTest {
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception {
+        Assert.fail("Not yet implemented");
+    }
+}
