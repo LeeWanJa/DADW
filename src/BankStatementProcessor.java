b28b084745c302d61c4b7f7fa87d5e1a75310686
이재완 <jjjkuul@naver.com>
2025-08-17 20:00:49 +0900

    예제 3-3, 반복 로직과 비즈니스 로직의 결합으로 인한 문제

--- src/BankStatementProcessor.java
+++ src/BankStatementProcessor.java
@@ -109,24 +109,15 @@ public class BankStatementProcessor {
     }
 
 
-    // 특정 금액 이상의 모든 입출력 내역을 검색하는 메서드
-    public List<BankTransaction> findTransactionsGreaterThanEquals(final int amount){
+    // 중복코드 문제 해결
+    // 특정 월이나 금액으로 입출금 내역 검색하기
+    public List<BankTransaction> findTransactionsInMonthAndGreater(final Month month, final int amount){
         final List<BankTransaction> result = new ArrayList<>();
         for(final BankTransaction bankTransaction : bankTransactions){
-            if(bankTransaction.getAmount() >= amount)
+            if(bankTransaction.getDate().getMonth() == month && bankTransaction.getAmount() >= amount)
                 result.add(bankTransaction);
         }
-        return result;
-    }
 
-    // 특정 월을 검색하는 메서드
-    // 이런식의 작성은 중복을 야기함
-    public List<BankTransaction> findTransactionsInMonth(final Month month){
-        final List<BankTransaction> result = new ArrayList<>();
-        for(final BankTransaction bankTransaction : bankTransactions){
-            if(bankTransaction.getDate().getMonth() == month)
-                result.add(bankTransaction);
-        }
         return result;
     }
 }
