a7be90be125005122482230c82c420a457a47963
이재완 <jjjkuul@naver.com>
2025-08-25 13:22:16 +0900

    예제 3-11 구현, 인터페이스 -> 암묵적 api -> 명시적 api

--- src/BankStatementProcessor.java
+++ src/BankStatementProcessor.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class BankStatementProcessor {
     private final List<BankTransaction> bankTransactions;
 
@@ -11,103 +12,18 @@ public class BankStatementProcessor {
         this.bankTransactions = bankTransactions;
     }
 
-    public double calculateTotalAmount(){
-        double total = 0;
-        for(final BankTransaction bankTransaction : bankTransactions){
-            total += bankTransaction.getAmount();
-        }
-        return total;
+    public double summarizeTransactions(final BankTransactionSummarizer bankTransactionSummarizer) {
+        double result = 0;
+        for(final BankTransaction bankTransaction : bankTransactions)
+            result = bankTransactionSummarizer.summarize(result, bankTransaction);
+
+        return result;
     }
 
     public double calculateTotalInMonth(final Month month){
-        double total = 0;
-        for(final BankTransaction bankTransaction : bankTransactions){
-            if(bankTransaction.getDate().getMonth() == month)
-                total += bankTransaction.getAmount();
-        }
-        return total;
+        return summarizeTransactions((acc, bankTransaction) ->
+                bankTransaction.getDate().getMonth() == month ? acc + bankTransaction.getAmount() : acc);
     }
-
-    public double calculateTotalForCategory(final String category){
-        double total = 0;
-        for(final BankTransaction bankTransaction : bankTransactions){
-            if(bankTransaction.getDescription().equals(category))
-                total += bankTransaction.getAmount();
-        }
-        return total;
-    }
-    
-    // 특정 날짜 범위에서 최대, 최소 거래 내역을 찾는 다양한 집계 연산 추가
-    // 반복문과 stream 둘 다로 구현해보자
-    public String findMaxMinTransactionDetails(Month from, Month to){
-        double max = Double.MIN_VALUE;
-        double min = Double.MAX_VALUE;
-
-        for(final BankTransaction bankTransaction : bankTransactions){
-            Month month = bankTransaction.getDate().getMonth();
-
-            if(!(from.compareTo(month) <= 0 && to.compareTo(month) >= 0))
-                continue;
-
-            double amount = bankTransaction.getAmount();
-
-            if(max < amount)
-                max = amount;
-
-            if(min > amount)
-                min = amount;
-        }
-
-        if(max == Double.MIN_VALUE && min == Double.MAX_VALUE)
-            return "no transactions";
-
-        return "max : " + max + " | min : " + min;
-    }
-
-    // 월별, 설명별 지출 그룹화 히스토그램
-    public Map<Month, Double> MonthHistogram(){
-        Map<Month, Double> monthMap = new HashMap<Month, Double>();
-
-        for(BankTransaction bankTransaction :  bankTransactions){
-            if(bankTransaction.getAmount() >= 0)
-                continue;
-
-            Month month = bankTransaction.getDate().getMonth();
-
-            // 키가 존재하는지 검사 후 존재한다면 기존 값에 덮어씌우기
-            // 존재하지 않는다면 새로운 객체를 만든 뒤 add
-//            if(monthMap.containsKey(month))
-//                monthMap.put(month, monthMap.get(month) + bankTransaction.getAmount());
-//            else
-//                monthMap.put(month, bankTransaction.getAmount());
-
-            // merge 사용
-            monthMap.merge(month, bankTransaction.getAmount(), (v1, v2) -> v1 + v2);
-        }
-
-        return monthMap;
-    }
-
-    public Map<String, Double> DescriptionHistogram(){
-        Map<String, Double> desMap = new HashMap<String, Double>();
-
-        for(BankTransaction bankTransaction :  bankTransactions){
-            if(bankTransaction.getAmount() >= 0)
-                continue;
-
-            String description = bankTransaction.getDescription();
-
-//            if(desMap.containsKey(description))
-//                desMap.put(description, desMap.get(description) + bankTransaction.getAmount());
-//            else
-//                desMap.put(description, bankTransaction.getAmount());
-
-            desMap.merge(description, bankTransaction.getAmount(), Double::sum);
-        }
-
-        return desMap;
-    }
-
 
     // 개방/폐쇄 원칙 적용
     public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter){
@@ -117,5 +33,9 @@ public class BankStatementProcessor {
                 result.add(bankTransaction);
         }
         return result;
+    }
+
+    public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount){
+        return findTransactions(bankTransaction -> bankTransaction.getAmount() >= amount);
     }
 }
